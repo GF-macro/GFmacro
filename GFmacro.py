ed9f8b0bf7147ba286d4efb592bb5eca3251455c
hello <hello>
2019-10-09 00:50:39 +0900

    y

--- GFmacro.py
+++ GFmacro.py
@@ -2,9 +2,12 @@ import pyautogui as pg
 import random
 import time
 import sys
+import threading
 from PyQt5.QtWidgets import *
 
 imgloc="./images/"
+imgstart=0
+imgfailed=0
 
 def ranclick(a):
     time.sleep(random.randrange(1,3))
@@ -14,35 +17,49 @@ def ranclick(a):
 
 
 def E24():
+    global imgstart,imgfailed
     imglist=["24E.png","commonFightButton.png","commandcenter.png","check.png","run"]
     try:
-        for i in imglist:
+        for num in range(imgstart,len(imglist)):
+            i=imglist[num]
+            '''
             if i=="commandcenter.png":
                 pg.move(-170,-270)
                 pg.click()
                 time.sleep(random.randrange(3,6))
                 continue
-            elif i=="run":
+            '''
+            if i=="run":
                 time.sleep(random.randrange(1,4))
                 pg.click()
-            a=None
             print(imgloc+i)
-            while True:
-                a=pg.locateCenterOnScreen(imgloc+i)
-                if a!=None:
-                    break
+            a=pg.locateCenterOnScreen(imgloc+i)                
             ranclick(a)
+            imgfailed=0
         
     except Exception as ex:
-        print("no image")
+        if imgfailed>=3:
+            print("----all failed----...")
+            return
+        print("no image %d/%d" %(imgfailed,4))
         print("error :"+str(ex))
+        time.sleep(1.4)
+        imgstart=num
+        imgfailed+=1
+        E24()
+
+def startthread():
+    global imgstart,imgfailed
+    imgstart=imgfailed=0
+    t=threading.Thread(target=E24)
+    t.start()
 class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
         self.layout=QVBoxLayout()
         self.button=QPushButton("2-4E")
-        self.button.clicked.connect(E24)
+        self.button.clicked.connect(startthread)
         self.initUI()
 
     def initUI(self):
